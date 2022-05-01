4339e678a20c7302d46a4ac6c7ad7292f29d2c5f
sanitysign <sanitysign@gmail.com>
2022-05-01 12:36:17 +0300

    relative path

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ const fetched = document.getElementById('fetched')
 imported.textContent = data.join(" ")
 
 document.addEventListener('click', async () => {
-  const data = await fetch("/mockData/statuses.json").then(res => res.json()).catch(() => "Couldn't load data")
+  const data = await fetch("../mockData/statuses.json").then(res => res.json()).catch(() => "Couldn't load data")
   console.log(data)
 
   if (typeof data === "string") return fetched.textContent = data
