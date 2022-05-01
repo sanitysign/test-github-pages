6c7e93e76ec7e256999d89f2812bc59eb2e54bd4
sanitysign <sanitysign@gmail.com>
2022-05-01 12:20:09 +0300

    added

---
+++ js/data.js
@@ -0,0 +1,6 @@
+export const data = [
+  1,
+  2,
+  3,
+  4,
+]
---
+++ js/main.js
@@ -0,0 +1,15 @@
+import {data} from "./data.js"
+
+const imported = document.getElementById('imported')
+const fetched = document.getElementById('fetched')
+
+imported.textContent = data.join(" ")
+
+document.addEventListener('click', async () => {
+  const data = await fetch("/mockData/statuses.json").then(res => res.json()).catch(() => "Couldn't load data")
+  console.log(data)
+
+  if (typeof data === "string") return fetched.textContent = data
+
+  fetched.textContent = data.map(({text}) => text).join(" ")
+})
