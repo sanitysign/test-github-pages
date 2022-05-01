331322abf3951342beb486a21300dc767c410654
sanitysign <sanitysign@gmail.com>
2022-05-01 13:44:43 +0300

    back to old paths

--- js/main.js
+++ js/main.js
@@ -6,9 +6,9 @@ const fetched = document.getElementById('fetched')
 imported.textContent = data.join(" ")
 
 document.addEventListener('click', async () => {
-  const data = await fetch("js/statuses.json").then(res => res.json()).catch(() => "Couldn't load data")
+  const data = await fetch("mockData/statuses.json").then(res => res.json()).catch(() => "Couldn't load data")
   console.log(data)
-
+ 
   if (typeof data === "string") return fetched.textContent = data
 
   fetched.textContent = data.map(({text}) => text).join(" ")
